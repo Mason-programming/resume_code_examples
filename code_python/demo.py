fb5fee84889cb3a23e96d734b818635b4f0847d4
Mason Kirby <masonkirby@Masons-Air.lan>
2025-03-05 13:00:24 -0800

    Adding test code

---
+++ code_python/demo.py
@@ -0,0 +1,5 @@
+"""
+The following are small snippets of private repository code.
+While these projects cant paint a full example, I hope it can
+demonstrate my prefered approaches to scenarios within python.
+"""
